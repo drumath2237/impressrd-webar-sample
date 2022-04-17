9292895541466e1b8d6dba668e5d91e6d0505856
drumath <2237k.drumath@gmail.com>
2022-04-17 18:02:34 +0900

    :sparkles: babylonを5系に更新＆ちょっと修正

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import './style.scss';
-import { Engine, MeshBuilder, Scene, WebXRHitTest } from '@babylonjs/core';
+import * as BABYLON from '@babylonjs/core';
 
 const main = async () => {
   const renderCanvas = <HTMLCanvasElement>(
@@ -7,25 +7,26 @@ const main = async () => {
   );
 
   if (renderCanvas) {
-    const engine = new Engine(renderCanvas, true);
-    const scene = new Scene(engine);
+    const engine = new BABYLON.Engine(renderCanvas, true);
+    const scene = new BABYLON.Scene(engine);
 
     scene.createDefaultCameraOrLight(true, true, true);
 
     const boxSize = 0.2;
-    const box = MeshBuilder.CreateBox('box', { size: boxSize });
+    const box = BABYLON.MeshBuilder.CreateBox('box', { size: boxSize });
     box.position.addInPlaceFromFloats(0, 1.6, 0);
 
     const xr = await scene.createDefaultXRExperienceAsync({
       uiOptions: {
         sessionMode: 'immersive-ar',
       },
+      optionalFeatures: true,
     });
     const featureManager = xr.baseExperience.featuresManager;
     const hitTest = featureManager.enableFeature(
-      WebXRHitTest,
+      BABYLON.WebXRHitTest,
       'latest',
-    ) as WebXRHitTest;
+    ) as BABYLON.WebXRHitTest;
 
     if (!hitTest) {
       return;
