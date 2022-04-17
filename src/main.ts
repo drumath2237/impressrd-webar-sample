623528c9bfd816f6178609de76a933e0c1621499
drumath <2237k.drumath@gmail.com>
2022-04-17 17:45:46 +0900

    :sparkles: hit testから姿勢情報を取得ができた

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import './style.scss';
-import * as BABYLON from '@babylonjs/core';
+import { Engine, MeshBuilder, Scene, WebXRHitTest } from '@babylonjs/core';
 
 const main = async () => {
   const renderCanvas = <HTMLCanvasElement>(
@@ -7,20 +7,37 @@ const main = async () => {
   );
 
   if (renderCanvas) {
-    const engine = new BABYLON.Engine(renderCanvas, true);
-    const scene = new BABYLON.Scene(engine);
+    const engine = new Engine(renderCanvas, true);
+    const scene = new Scene(engine);
 
     scene.createDefaultCameraOrLight(true, true, true);
-    // scene.createDefaultEnvironment();
 
     const boxSize = 0.2;
-    const box = BABYLON.MeshBuilder.CreateBox('box', { size: boxSize });
+    const box = MeshBuilder.CreateBox('box', { size: boxSize });
     box.position.addInPlaceFromFloats(0, 1.6, 0);
 
-    await scene.createDefaultXRExperienceAsync({
+    const xr = await scene.createDefaultXRExperienceAsync({
       uiOptions: {
         sessionMode: 'immersive-ar',
       },
+    });
+    const featureManager = xr.baseExperience.featuresManager;
+    const hitTest = featureManager.enableFeature(
+      WebXRHitTest,
+      'latest',
+    ) as WebXRHitTest;
+
+    if (!hitTest) {
+      return;
+    }
+
+    hitTest.onHitTestResultObservable.add((result) => {
+      if (!result.length) {
+        return;
+      }
+
+      box.position = result[0].position;
+      box.rotationQuaternion = result[0].rotationQuaternion;
     });
 
     engine.runRenderLoop(() => {
