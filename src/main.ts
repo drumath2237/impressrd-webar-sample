406e5d452c1a5652fd7d9416c579028e36c4cea1
drumath <2237k.drumath@gmail.com>
2022-04-20 00:58:04 +0900

    :sparkles: hitの場所にcubeを配置する実装完了

--- src/main.ts
+++ src/main.ts
@@ -20,21 +20,6 @@ const main = async () => {
 
     scene.createDefaultCameraOrLight(true, true, true);
 
-    // GUI settings
-    const button = GUI.Button.CreateSimpleButton('button', 'Put');
-    button.widthInPixels = 800;
-    button.heightInPixels = 150;
-    button.color = 'white';
-    button.cornerRadius = 20;
-    button.background = 'green';
-    button.fontSizeInPixels = 50;
-    button.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
-    button.topInPixels = -10;
-
-    GUI.AdvancedDynamicTexture.CreateFullscreenUI('UI', true, scene).addControl(
-      button,
-    );
-
     const boxSize = 0.2;
     const box = BABYLON.MeshBuilder.CreateBox('box', { size: boxSize });
     box.visibility = 0;
@@ -46,25 +31,57 @@ const main = async () => {
       'latest',
     ) as BABYLON.WebXRHitTest;
 
-    // if (!hitTest) {
-    //   return;
-    // }
+    let latestHitTestResult: BABYLON.IWebXRHitResult | null = null;
+    hitTest?.onHitTestResultObservable.add((result) => {
+      if (!result.length) {
+        latestHitTestResult = null;
+        box.visibility = 0;
+        return;
+      }
 
-    // hitTest.onHitTestResultObservable.add((result) => {
-    //   if (!result.length) {
-    //     box.visibility = 0;
-    //     return;
-    //   }
+      latestHitTestResult = result[0];
 
-    //   box.position = result[0].position;
-    //   box.rotationQuaternion = result[0].rotationQuaternion;
-    //   box.visibility = 1;
-    // });
+      box.position = result[0].position;
+      box.rotationQuaternion = result[0].rotationQuaternion;
+      box.visibility = 1;
+    });
+
+    const button = addButton();
+    button.onPointerClickObservable.add(() => {
+      if (latestHitTestResult === null) {
+        return;
+      }
+
+      const hitbox = box.clone('hitbox');
+      hitbox.position = latestHitTestResult.position;
+      hitbox.rotationQuaternion = latestHitTestResult.rotationQuaternion;
+    });
 
     engine.runRenderLoop(() => {
       scene.render();
     });
   }
+};
+
+const addButton = () => {
+  // GUI settings
+  const button = GUI.Button.CreateSimpleButton('button', 'Put');
+  button.widthInPixels = 800;
+  button.heightInPixels = 150;
+  button.color = 'white';
+  button.cornerRadius = 20;
+  button.background = 'green';
+  button.fontSizeInPixels = 50;
+  button.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+  button.topInPixels = -10;
+
+  const advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI(
+    'UI',
+    true,
+  );
+  advancedTexture.addControl(button);
+
+  return button;
 };
 
 main();
